a9d4c3b23f6c365ea93c47a0f0d0d85eeabc1256
ShallowDream <shallowdream@ShallowDreamdeMacBook-Pro.local>
2023-08-29 16:30:17 +0800

    1

--- ex_count.py
+++ ex_count.py
@@ -1,42 +1,17 @@
-import json
-import matplotlib.pyplot as plt
+def reorder_points(points):
+    # 首先按纵坐标排序
+    sorted_points = sorted(points, key=lambda p: p[1])
+    
+    reordered_points = []
+    while sorted_points:
+        # 从开始和结尾取点，并将其添加到新列表中
+        if sorted_points:
+            reordered_points.append(sorted_points.pop(0))
+        if sorted_points:
+            reordered_points.append(sorted_points.pop())
+    
+    return reordered_points
 
-# 从文件中读取数据
-with open('your_data_file.json', 'r') as file:
-    data = json.load(file)
-
-# 使用字典来统计每个exer_id被多少个user_id涉及
-count_dict = {}
-
-for user_data in data:
-    done_exer_ids = set()  # 用于确保一个user_id只计算一次
-    for log in user_data["logs"]:
-        exer_id = log["exer_id"]
-        if exer_id not in done_exer_ids:
-            count_dict[exer_id] = count_dict.get(exer_id, 0) + 1
-            done_exer_ids.add(exer_id)
-
-# 对exer_ids进行排序
-sorted_exer_ids = sorted(list(count_dict.keys()))
-
-# 按每10个exer_id合并为一个区间，并加总用户数
-merged_exer_intervals = []
-merged_user_counts = []
-for i in range(0, len(sorted_exer_ids), 10):
-    start = sorted_exer_ids[i]
-    end = sorted_exer_ids[min(i + 9, len(sorted_exer_ids) - 1)]
-    merged_exer_intervals.append(f"{start}-{end}")
-    merged_user_counts.append(sum([count_dict[exer_id] for exer_id in sorted_exer_ids[i:i+10]]))
-
-plt.bar(merged_exer_intervals, merged_user_counts)
-plt.xlabel('Exer ID Interval')
-plt.ylabel('Number of Users')
-plt.title('Number of Users for each Exer ID Interval')
-plt.xticks(rotation=45)
-plt.tight_layout()
-
-# 保存图像
-plt.savefig('output_merged_filename.png', dpi=300, bbox_inches='tight')
-
-# 显示图像
-plt.show()
+# 示例数据：[(x1, y1), (x2, y2), ...]
+points = [(1, 5), (2, 3), (3, 8), (4, 2), (5, 7)]
+print(reorder_points(points))
